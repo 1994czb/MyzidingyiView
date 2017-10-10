1fb99915abcba7d33dcd1cbdc3a7ec48080a90d4
caozhongbin <861088350@qq.com>
2017-10-10 15:10:53 +0800

    自定义画三角形

---
+++ my自定义画三角形/src/main/java/com/example/sanjiaoxing/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sanjiaoxing;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MyView myView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myView = (MyView) findViewById(R.id.myview);
+
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.aaa);
+        myView.setBitmap(bitmap);
+    }
+}
